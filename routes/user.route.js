a4e76b250e8cd83767fbca321bed97c154b2185b
chrisdresba <chrisdresba@gmail.com>
2023-05-16 22:44:45 -0300

    primer-commit

--- routes/user.route.js
+++ routes/user.route.js
@@ -5,19 +5,47 @@ const {
   usersPut,
   usersPost,
   usersDelete,
-  usersPatch,
 } = require("../controllers/user.controller");
+const { check } = require("express-validator");
+const { validateFields } = require("../middlewares/validate-fields");
+const {
+  isValidRole,
+  existEmail,
+  userExistsById,
+} = require("../helpers/db-validators");
 
 const router = Router();
 
 router.get("/", usersGet);
 
-router.post("/", usersPost);
+router.post(
+  "/",
+  [
+    check("email", "El correo no es valido").isEmail(),
+    check("email").custom(existEmail),
+    check("name", "El nombre es obligatorio").not().isEmpty(),
+    check("password", "El password debe de ser mas de 6 letras").isLength({
+      min: 6,
+    }),
+    check("role").custom(isValidRole),
+    validateFields,
+  ],
+  usersPost
+);
 
-router.put("/:id", usersPut);
+router.put(
+  "/:id",
+  [check("id", "No es un ID válido").isMongoId(), validateFields],
+  check("id").custom(userExistsById),
+  check("role").custom(isValidRole),
+  usersPut
+);
 
-router.patch("/", usersPatch);
-
-router.delete("/:id", usersDelete);
+router.delete(
+  "/:id",
+  check("id", "No es un ID válido").isMongoId(),
+  check("id").custom(userExistsById),
+  usersDelete
+);
 
 module.exports = router;
