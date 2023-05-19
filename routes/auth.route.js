1fd994c26711d5ee04f48c37d16dc594f79779cb
chrisdresba <chrisdresba@gmail.com>
2023-05-18 22:46:57 -0300

    Fin sección 11 - version 4.0.0

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -1,6 +1,6 @@
 const { Router } = require("express");
 const { check } = require("express-validator");
-const { login } = require("../controllers/auth.controller");
+const { login, googleSignIn } = require("../controllers/auth.controller");
 const { validateFields } = require("../middlewares/validate-fields");
 
 const router = Router();
@@ -13,6 +13,12 @@ router.post(
     validateFields,
   ],
   login
+);
+
+router.post(
+  "/google",
+  [check("id_token", "id_token es necesario").not().isEmpty(), validateFields],
+  googleSignIn
 );
 
 module.exports = router;
