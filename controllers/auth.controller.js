341002c45fa7b607cfb7e54d8ab1280bbc7aef54
chrisdresba <chrisdresba@gmail.com>
2023-05-18 22:46:57 -0300

    Fin sección 11 - version 4.0.0

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,35 +1,37 @@
 const { response } = require("express");
 const User = require("../models/user");
 const bcrypt = require("bcryptjs");
+
 const { generateJWT } = require("../helpers/generate-jwt");
+const { googleVerify } = require("../helpers/google-verify");
 
 const login = async (req, res = response) => {
   const { email, password } = req.body;
 
   try {
-    const usuario = await User.findOne({ email });
+    const user = await User.findOne({ email });
     //check exist user
-    if (!usuario) {
+    if (!user) {
       return res
         .status(400)
         .json({ msg: "Usuario / password no son correctos" });
     }
     //check state
-    if (!usuario.state) {
+    if (!user.state) {
       return res.status(400).json({ msg: "Usuario inactivo" });
     }
 
-    const validPassword = bcrypt.compareSync(password, usuario.password);
+    const validPassword = bcrypt.compareSync(password, user.password);
     if (!validPassword) {
       return res
         .status(400)
         .json({ msg: "Usuario / password no son correctos" });
     }
 
-    const token = await generateJWT(usuario.id);
+    const token = await generateJWT(user.id);
 
     res.json({
-      usuario,
+      user,
       token,
     });
   } catch (error) {
@@ -37,6 +39,51 @@ const login = async (req, res = response) => {
     return res.status(500).json({ msg: "Error, contactar al administrador" });
   }
 };
+
+const googleSignIn = async (req, res = response) => {
+  const { id_token } = req.body;
+
+  try {
+    const { name, email, img } = await googleVerify(id_token);
+
+    let user = await User.findOne({ email });
+
+    if (!user) {
+      //create user
+      const data = {
+        name,
+        email,
+        password: "x",
+        img,
+        google: true,
+        role: "USER_ROLE",
+      };
+
+      user = new User(data);
+      await user.save();
+    }
+
+    if (!user.state) {
+      return res.status(401).json({
+        msg: "Hable con el administrador, usuario bloqueado",
+      });
+    }
+
+    //generate JWT
+    const token = await generateJWT(user.id);
+
+    res.json({
+      user,
+      token,
+    });
+  } catch (error) {
+    res.status(400).json({
+      msg: "Token de google no es valido",
+    });
+  }
+};
+
 module.exports = {
   login,
+  googleSignIn,
 };
